4371ba3050d3bee7782089a583fe8d0a6ba93ee2
Mohamad <khajezade.mohamad@gmail.com>
2020-07-03 02:13:14 +0430

    Adding other learners

--- tests/lazy/test_assignment_evaluation.py
+++ tests/lazy/test_assignment_evaluation.py
@@ -8,6 +8,8 @@ import pytest   # noqa
 from skmultiflow.data.data_stream import DataStream
 from skmultiflow.lazy import KNNClassifier, WeightedKNNClassifier
 from skmultiflow.evaluation import EvaluatePrequential
+from skmultiflow.trees import HoeffdingTreeClassifier
+from skmultiflow.bayes import NaiveBayes
 
 
 def test_data_stream(test_path):
@@ -30,6 +32,11 @@ def test_data_stream(test_path):
         leaf_size=40,
         standardize=True
     )
+    nominal_attr_idx = [x for x in range(15, len(stream.feature_names))]
+
+    hoeffding_learner = HoeffdingTreeClassifier(nominal_attributes=nominal_attr_idx)
+    nb_learner = NaiveBayes()
+
     metrics = ['accuracy', 'kappa_m', 'kappa_t', 'recall']
     output_file = os.path.join(test_path, 'data/kkn_output.csv')
     evaluator = EvaluatePrequential(metrics=metrics, output_file=output_file)
@@ -40,9 +47,12 @@ def test_data_stream(test_path):
         model=[
             normal_knn_learner,
             weighted_knn_learner,
-            standardize_knn_learner
+            standardize_knn_learner,
+            hoeffding_learner,
+            nb_learner,
         ]
     )
     mean_performance, current_performance = evaluator.get_measurements()
+    import pudb; pudb.set_trace()  # XXX BREAKPOINT
     assert 1 == 1
 
